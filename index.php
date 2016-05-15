6bd044868abf446f5d2e47bd009e8aa9ca61248f
dmitry <zrayev@mail.ru>
2016-05-15 22:21:28 +0300

    add Right Triangle

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Triangle\EquilateralTriangle;
 use Triangle\IsoscelesTriangle;
 use Triangle\ScaleneTriangle;
 
-echo 'EquilateralTriangle' . "<br>\n";
+echo 'Equilateral Triangle' . "<br>\n";
 $equilateral = new EquilateralTriangle();
 $equilateral ->setLengthA(5);
 $equilateral ->setLengthB(5);
@@ -13,7 +13,7 @@ $equilateral ->setLengthC(5);
 
 echo $equilateral->printScreen() . "<br>\n";
 
-echo 'IsoscelesTriangle' . "<br>\n";
+echo 'Isosceles Triangle' . "<br>\n";
 $isosceles = new IsoscelesTriangle();
 $isosceles ->setLengthA(10);
 $isosceles ->setLengthB(10);
@@ -21,10 +21,18 @@ $isosceles ->setLengthC(18);
 
 echo $isosceles->printScreen() . "<br>\n";
 
-echo 'ScaleneTriangle' . "<br>\n";
+echo 'Scalene Triangle' . "<br>\n";
 $scalene = new ScaleneTriangle();
 $scalene ->setLengthA(5);
 $scalene ->setLengthB(6);
 $scalene ->setLengthC(7);
 
 echo $scalene->printScreen() . "<br>\n";
+
+echo 'Right Triangle' . "<br>\n";
+$right = new ScaleneTriangle();
+$right ->setLengthA(12);
+$right ->setLengthB(16);
+$right ->setLengthC(20);
+
+echo $right->printScreen() . "<br>\n";
